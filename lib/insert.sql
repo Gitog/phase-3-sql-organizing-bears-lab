b9763a21ac584b0694dae596000d4b15397a8c98
James <jamesnjorio@gmail.com>
2022-11-07 17:35:29 +0300

    lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Mr. Chocolate", 12, "M","Brown","Giggy", 1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Rowdy", 2, "M", "Gray", "QUiet", 0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Tabitha", 4, "F", "Pale GRAY", "Brave", 1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Sergeant Brown", 5, "M", "Dark Brown", "Courageos", 1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Melissa", 3, "F", "Black", "Easy", 0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Grinch", 4, "M", "Dark gray", "Calm", 1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Wendy", 6, "F", "WHITE", "Soft", 1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (NULL, 20, "M", "Black", "Brave", 0)
